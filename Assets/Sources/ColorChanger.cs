71698c899b2f4ebd4d16858beb8c02ab3ec65fbb
Jan Rabe <jan.rabe@wooga.net>
2014-03-14 13:13:03 +0100

    added detonator

--- Assets/Sources/ColorChanger.cs
+++ Assets/Sources/ColorChanger.cs
@@ -3,11 +3,26 @@ using System.Collections;
 
 public class ColorChanger : MonoBehaviour {
 
+	public Color Color;
+	private Color defaultColor;
+
+	public void Awake() {
+		defaultColor = renderer.material.color;
+	}
+
 	void Start () {
-		
+		// renderer.materials [0].color = Color.blue;
+		//renderer.materials [0].SetColor ("_Color", Color.blue);
 	}
 	
 	void Update () {
-	
+	}
+
+	public void OnMouseDown() {
+		gameObject.GetComponentInChildren<Renderer>().material.color = Color;
+	}
+
+	public void OnMouseUp() {
+		gameObject.GetComponentInChildren<Renderer>().material.color = defaultColor;
 	}
 }
---
+++ Assets/Sources/Mover.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Mover : MonoBehaviour {
+
+	public float Speed;
+
+	void Start () {
+		
+	}
+
+	void OnGizmo() {
+		Debug.DrawRay (transform.position, transform.up, Color.green);
+		Debug.DrawRay (transform.position, transform.forward, Color.blue);
+	}
+
+	void Update () {
+		GetComponent<Transform>().position = Vector3.up * Time.time * Speed * Input.GetAxis("Vertical");
+	}
+}
